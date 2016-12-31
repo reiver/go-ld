a002ea7bf51bb068812c392849e53b1f500589db
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-31 14:19:43 -0800

    ld.Int64

--- int64_notloaded.go
+++ int64_notloaded.go
@@ -2,6 +2,10 @@ package ld
 
 // Int64Some returns an Int64 which does not have a value;
 // and in particular the "not loaded" type of a lack of a value.
+//
+// For example:
+//
+//	var x ld.Int64 = ld.Int64NotLoaded()
 func Int64NotLoaded() Int64 {
 	return Int64{}
 }
--- int64_some.go
+++ int64_some.go
@@ -1,6 +1,10 @@
 package ld
 
-// Int64Some returns an Int64 with the value.
+// Int64Some returns an Int64 with a value.
+//
+// For example:
+//
+//	var x ld.Int64 = ld.Int64Some(41)
 func Int64Some(v int64) Int64 {
 	return Int64 {
 		value: v,
