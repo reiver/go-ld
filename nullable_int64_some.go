c8c294856bec12d0820331d5b3c14b1f95cff82a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-31 15:22:03 -0800

    ld.NullableInt64

---
+++ nullable_int64_some.go
@@ -0,0 +1,14 @@
+package ld
+
+// NullableInt64Some returns a NullableInt64 with a value.
+//
+// For example:
+//
+//	var x ld.NullableInt64 = ld.NullableInt64Some(41)
+func NullableInt64Some(v int64) NullableInt64 {
+	return Int64 {
+		value: v,
+		loaded: true,
+		null: false,
+	}
+}
